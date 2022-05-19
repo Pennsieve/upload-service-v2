9e6b5ce4f47bd90e4650e32395288cbdb09c7dcb
Joost Wagenaar <1605953+jwagenaar@users.noreply.github.com>
2022-05-18 22:37:50 -0400

    moving things around

--- lambda/upload/go.mod
+++ lambda/upload/go.mod
@@ -2,22 +2,27 @@ module github.com/pennsieve/pennsieve-upload-service-v2/upload
 
 go 1.18
 
+replace github.com/pennsieve/pennsieve-go-api => ../../../pennsieve-go-api
+
 require (
-	github.com/aws/aws-lambda-go v1.31.1
-	github.com/aws/aws-sdk-go-v2/config v1.15.5
-	github.com/aws/aws-sdk-go-v2/feature/rds/auth v1.1.20
-	github.com/lib/pq v1.10.5
+	github.com/aws/aws-lambda-go v1.32.0
+	github.com/aws/aws-sdk-go v1.44.15
+	github.com/google/uuid v1.3.0
+	github.com/pennsieve/pennsieve-go-api v0.0.2
 )
 
 require (
-	github.com/aws/aws-sdk-go-v2 v1.16.3 // indirect
-	github.com/aws/aws-sdk-go-v2/credentials v1.12.0 // indirect
-	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.12.4 // indirect
-	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.10 // indirect
-	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.4.4 // indirect
-	github.com/aws/aws-sdk-go-v2/internal/ini v1.3.11 // indirect
-	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.9.4 // indirect
-	github.com/aws/aws-sdk-go-v2/service/sso v1.11.4 // indirect
-	github.com/aws/aws-sdk-go-v2/service/sts v1.16.4 // indirect
+	github.com/aws/aws-sdk-go-v2 v1.16.4 // indirect
+	github.com/aws/aws-sdk-go-v2/config v1.15.7 // indirect
+	github.com/aws/aws-sdk-go-v2/credentials v1.12.2 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.12.5 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/rds/auth v1.1.21 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.11 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.4.5 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.3.12 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.9.5 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.11.5 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.16.6 // indirect
 	github.com/aws/smithy-go v1.11.2 // indirect
+	github.com/lib/pq v1.10.6 // indirect
 )
--- lambda/upload/handler/handler.go
+++ lambda/upload/handler/handler.go
@@ -2,21 +2,149 @@ package handler
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/google/uuid"
+	"github.com/pennsieve/pennsieve-go-api/config"
+	"github.com/pennsieve/pennsieve-go-api/models"
+	"github.com/pennsieve/pennsieve-go-api/models/db"
+	"github.com/pennsieve/pennsieve-go-api/models/packageInfo"
 	"log"
 )
+
+// UploadSession contains the information that is shared based on the upload session ID
+type UploadSession struct {
+	OrganizationId int
+	DatasetId      int
+	OwnerId        int
+}
 
 func Handler(ctx context.Context, sqsEvent events.SQSEvent) error {
 	for _, message := range sqsEvent.Records {
 		log.Printf("The message %s for event source %s = %s \n\n", message.MessageId, message.EventSource, message.Body)
 	}
 
-	db, _ := connectRDS()
+	// 1. Connect to Database
+	conn, _ := config.ConnectRDS()
+	defer func() {
+		err := conn.Close()
+		if err != nil {
+			log.Println("Unable to close DB connection from Lambda function.")
+			return
+		}
+		log.Println("Closing DB connection.")
+	}()
 
-	// Close database connection.
-	if db.Ping() == nil {
-		db.Close()
+	// 2. Retrieve info from Manifest Tables
+	// TODO: Replace this with API calls
+	session := UploadSession{
+		OrganizationId: 19,   // Pennsieve Test
+		DatasetId:      1682, // Test Upload
+		OwnerId:        24,   // Joost
+	}
+
+	// Parse uploadFiles from S3 Event
+	uploadFiles, _ := getUploadFiles(sqsEvent.Records)
+
+	// Map by uploadSessionID
+	var fileBySession = map[string][]models.UploadFile{}
+	for _, f := range uploadFiles {
+		fileBySession[f.SessionId] = append(fileBySession[f.SessionId], f)
+	}
+
+	for _, fileUploads := range fileBySession {
+		importFiles(fileUploads, session)
 	}
 
 	return nil
+}
+
+func getUploadFiles(fileEvents []events.SQSMessage) ([]models.UploadFile, error) {
+
+	var pkgs []models.UploadFile
+	for _, message := range fileEvents {
+
+		/*
+			Messages can be from multiple upload sessions --> multiple organizations.
+			We need to:
+				1. Separate by upload-session
+				2. Create/get the folders from postgres for each upload session
+				3. Create Packages
+				4. Create Files in Packages
+		*/
+
+		parsedS3Event := events.S3Event{}
+		if err := json.Unmarshal([]byte(aws.StringValue(&message.Body)), &parsedS3Event); err != nil {
+			return nil, fmt.Errorf("failed to unmarshal message, %v", err)
+		}
+
+		// Get UploadFile Representation from event
+		var uf = models.UploadFile{}
+		uploadFile, _ := uf.FromS3Event(&parsedS3Event)
+
+		pkgs = append(pkgs, uploadFile)
+
+	}
+
+	return pkgs, nil
+}
+
+func getPackageParams(uploadFiles []models.UploadFile, session UploadSession) ([]db.PackageParams, error) {
+	var pkgParams []db.PackageParams
+
+	for _, file := range uploadFiles {
+		packageID := fmt.Sprintf("N:package:%s", uuid.New().String())
+
+		// TODO: Figure out how to handle Parent ID and Folders
+		parentId := -1 // Setting to -1 identifies the root of the dataset
+
+		//TODO: Replace by s3Key when mapped
+		uploadId := uuid.New().String()
+
+		// Set Default attributes for File ==> Subtype and Icon
+		var attributes []models.FileAttribute
+		attributes = append(attributes, models.FileAttribute{
+			Key:      "subType",
+			Fixed:    false,
+			Value:    file.SubType,
+			Hidden:   true,
+			Category: "Pennsieve",
+			DataType: "string",
+		}, models.FileAttribute{
+			Key:      "icon",
+			Fixed:    false,
+			Value:    file.Icon.String(),
+			Hidden:   true,
+			Category: "Pennsieve",
+			DataType: "string",
+		})
+
+		pkgParam := db.PackageParams{
+			Name:         file.Name,
+			PackageType:  file.Type,
+			PackageState: packageInfo.Uploaded,
+			NodeId:       packageID,
+			ParentId:     parentId,
+			DatasetId:    session.DatasetId,
+			OwnerId:      session.OwnerId,
+			Size:         file.Size,
+			ImportId:     uploadId,
+			Attributes:   attributes,
+		}
+
+		pkgParams = append(pkgParams, pkgParam)
+	}
+
+	return pkgParams, nil
+
+}
+
+func importFiles(files []models.UploadFile, session UploadSession) {
+	pkgParams, _ := getPackageParams(files, session)
+
+	var pkg db.Package
+	pkg.Add(session.OrganizationId, pkgParams)
+
 }
