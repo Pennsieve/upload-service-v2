3328125db1d0b4bf971f615b9a44ab1cb7295667
Joost Wagenaar <1605953+jwagenaar@users.noreply.github.com>
2023-03-27 12:04:08 -0400

    allowing upload service to delete manifest rows

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -227,6 +227,7 @@ data "aws_iam_policy_document" "upload_service_v2_iam_policy_document" {
     effect = "Allow"
 
     actions = [
+      "dynamodb:DeleteItem",
       "dynamodb:DescribeTable",
       "dynamodb:BatchGetItem",
       "dynamodb:GetItem",
