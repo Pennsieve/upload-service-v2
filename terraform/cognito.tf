0c3dd7b122f37f824c15d519c063db93c97f93e2
Joost Wagenaar <1605953+jwagenaar@users.noreply.github.com>
2024-03-07 16:56:57 -0500

    fix tf

--- terraform/cognito.tf
+++ terraform/cognito.tf
@@ -3,14 +3,14 @@ resource "aws_cognito_identity_pool" "pennsieve_auth" {
   allow_unauthenticated_identities = false
   allow_classic_flow               = false
 
-  cognito_identity_providers {
+  cognito_identity_providers  {
     client_id               = data.terraform_remote_state.authentication_service.outputs.token_pool_client_id
     provider_name           = "cognito-idp.${var.aws_region}.amazonaws.com/${data.terraform_remote_state.authentication_service.outputs.token_pool_id}"
     server_side_token_check = false
   }
 
   cognito_identity_providers {
-    client_id               = data.terraform_remote_state.authentication_service.outputs.user_pool_2_id
+    client_id               = data.terraform_remote_state.authentication_service.outputs.user_pool_2_client_id
     provider_name           = "cognito-idp.${var.aws_region}.amazonaws.com/${data.terraform_remote_state.authentication_service.outputs.user_pool_2_id}"
     server_side_token_check = false
   }
